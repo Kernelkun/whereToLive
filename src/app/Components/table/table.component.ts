80526235b8cf3c20336c7fdb920ac17592eaf28b
José Gázquez <kernel.kun@gmail.com>
2018-01-14 11:24:00 +0100

    I tried to add some arrows to show when the time is up or above the initial time. We have some issues with the promises, but I found that exists something called forkJoin. ForkJoin do al the promises and return one Array when all has been completed.

--- src/app/Components/table/table.component.ts
+++ src/app/Components/table/table.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { GoogleDirectionsService } from '../../Services/google-directions.service';
+import {Observable} from 'rxjs/Observable';
+import { forkJoin } from "rxjs/observable/forkJoin";
 
 @Component({
   selector: 'app-table',
@@ -11,6 +13,7 @@ export class TableComponent implements OnInit {
   stops = [];
   places = [];
   destinations;
+  complete: number;
 
   constructor(private _googleDirections: GoogleDirectionsService) {
     this.origin = _googleDirections.getOrigin();
@@ -25,17 +28,24 @@ export class TableComponent implements OnInit {
 
   getTimes(origin: string, key: number) {
     this.stops.forEach((item, index) => {
-      this._googleDirections.getTimeFromTo(origin, item.address).subscribe((res) => {
-        this.destinations[key][item.name] = (Math.round(((res.routes[0].legs[0].duration.value / 60) + 0.00001) * 100) / 100) * 2;
+      this._googleDirections.getTimeFromTo(origin, item.address).subscribe(
+        (res) => {
+          this.destinations[key][item.name] = (Math.round(((res.routes[0].legs[0].duration.value / 60) + 0.00001) * 100) / 100) * 2;
 
-        this.destinations[key].total = Math.round((this.destinations[key].work + this.destinations[key].dojo + 0.00001) * 100) / 100;
+          this.destinations[key].total = Math.round((this.destinations[key].work + this.destinations[key].dojo + 0.00001) * 100) / 100;
 
-        const number = this.destinations[key].total - this.destinations[0].total;
+          const number = this.destinations[key].total - this.destinations[0].total;
 
-        this.destinations[key].dif = Math.round((number + 0.00001) * 100) / 100;
+          this.destinations[key].dif = Math.round((number + 0.00001) * 100) / 100;
 
-        // this.destinations[0].dif = 0;
-      });
+          // this.destinations[0].dif = 0;
+        },
+        (error) => console.log(error),
+        () => {
+          // console.log(this.destinations[key].name + ' complete!');
+          this.complete++;
+        }
+      );
     });
   }
 
@@ -43,6 +53,16 @@ export class TableComponent implements OnInit {
     this.destinations.forEach((item, index) => {
       this.getTimes(item.address, index);
     });
+  }
+
+  compareTimes(item, key) {
+    if ( key > 0 ) {
+      if (this.destinations[key][item.name] > this.destinations[0][item.name]) {
+        this.destinations[key].comparision = "down";
+      } else {
+        this.destinations[key].comparision = "up";
+      }
+    }
   }
 
 }
